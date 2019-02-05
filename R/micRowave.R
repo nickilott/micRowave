367b4971391ce90cea0bfebbf15e59fb435f9428
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2019-02-05 02:30:27 +0000

    adding animation options

--- R/micRowave.R
+++ R/micRowave.R
@@ -277,7 +277,7 @@ microwave <- function(abundances, with_image=TRUE, animation=FALSE, background="
 	library(dplyr)
         to.animate <- bind_rows(dfs) 
         p <- plotWave(to.animate, colours, g=g, with_image=with_image, background=background, text=text)
-	p <- p + labs(title=frame_time) + transition_manual(sample) 
+	p <- p  + transition_manual(sample) 
         anim_save(p, filename="allwaves.gif")
     }
 }
